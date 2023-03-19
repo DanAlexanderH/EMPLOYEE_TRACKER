ea3fc570e0f282d1b311001c1634d11ccb985ed1
Daniel <ttdanny98@gmail.com>
2023-03-19 11:10:43 -0700

    added view all employees function to inquery

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 const dbConect = require("./connection/connection")
 const inquirer = require("Inquirer");
-const fs = require("fs");
 
 dbConect.connect((error) => {
     if (error) {
         // throw error
         console.log("Could not connect to database!")
     } else {
-        console.log("Success!")
+        console.log("\nWelcome to Employee Tracker!\n")
+        prompt();
     }
 })
 
+const prompt = () => {
+    inquirer.prompt ([
+    {
+        name: "choices",
+        type: "list",
+        message: "Select an option",
+        choices: [
+            "View all Employees"
+        ]
+    }
+])
+.then((answers) => {
+    const {choices} = answers;
 
-// inquirer
-// .prompt ([
-//     {
-//         type: "input",
-//         name: "test",
-//         message: "This is a test prompt"
+    if (choices === "View all Employees") {
+        viewAllEmployees();
+    } else {
+        return 
+    }
+});
+};
 
-//     }
-// ])
+const viewAllEmployees = () => {
+    let mysql = `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS "department", roles.salary
+                FROM employee, roles, department
+                WHERE department.id = roles.department_id AND roles.id = employee.role_id`;
+    dbConect.query(mysql, (err, res) => {
+        if (err) {
+            throw err
+        } else {
+            console.log(res);
+        }
+        prompt();
+    })
+}
+
+
